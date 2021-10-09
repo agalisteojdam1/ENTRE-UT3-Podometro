bebf4229388f0d10105361ec8ce2850b0aa0ab68
Asier Galisteo <agalisteojdam1@gmail.com>
2021-10-09 17:36:58 +0200

    Atributos, constantes, constructor y getMarca()

--- Podometro.java
+++ Podometro.java
@@ -30,8 +30,18 @@ public class Podometro {
      * Inicializa el podómetro con la marca indicada por el parámetro.
      * El resto de atributos se ponen a 0 y el sexo, por defecto, es mujer
      */
-    public Podometro() {
-
+    public Podometro(String queMarca) {
+        marca = queMarca;
+        altura = 0;
+        sexo = 'M';
+        longitudZancada = 0;
+        totalPasosLaborables = 0;
+        totalPasosSabado = 0;
+        totalPasosDomingo = 0;
+        totalDistanciaSemana = 0;
+        totalDistanciaFinSemana = 0;
+        minutos = 0;
+        caminatasNoche = 0;
         
     }
 
